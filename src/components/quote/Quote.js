29810991c8064de09a5b880c585e61a99de60876
Marco <marcoparra311@gmail.com>
2021-08-17 16:29:36 -0400

    Fix linters

--- src/components/quote/Quote.js
+++ src/components/quote/Quote.js
@@ -2,20 +2,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Quote = () => {
-  return (
-    <div className="quote-container">
-      <p className="quoteText"> 
-        Mathematics is not about numbers, equations, computations, or algorithms:
-        It is about understanding.
-      </p>
-      <cite className="cite">
-        <Link to="https://es.wikipedia.org/wiki/William_Thurston" id="link">
-        - William Paul Thurston  
-        </Link>
-      </cite>
-    </div>
-  )
-}
+const Quote = () => (
+  <div className="quote-container">
+    <p className="quoteText">
+      Mathematics is not about numbers, equations, computations, or algorithms:
+      It is about understanding.
+    </p>
+    <cite className="cite">
+      <Link to="https://es.wikipedia.org/wiki/William_Thurston" id="link">
+        - William Paul Thurston
+      </Link>
+    </cite>
+  </div>
+);
 
 export default Quote;
